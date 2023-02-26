0d7cce5b161b4aca769f4d07d337f85d7d31ffec
KRMadhusanka99 <Kkrmadhu1999@gmail.com>
2023-02-27 03:43:31 +0530

    'styleWorkRecord'

--- src/components/Input_Docs/InputBox.js
+++ src/components/Input_Docs/InputBox.js
@@ -3,7 +3,7 @@ import Style from '../Input_Docs/InputBox.module.css'
 
 const InputBox = () => {
     return ( 
-        <div>
+        <div className={Style.oute_div}>
             <input className={Style.input_style} type="text" placeholder='Input Text' />
             <button type="button" className={Style.button_style}>ADD</button>
         </div>
