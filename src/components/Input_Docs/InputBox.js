8005f5cdce4e224b6f2421d01fb0d0a5e0b617e6
KRMadhusanka99 <Kkrmadhu1999@gmail.com>
2023-02-27 03:02:16 +0530

    'styleInputField'

--- src/components/Input_Docs/InputBox.js
+++ src/components/Input_Docs/InputBox.js
@@ -4,7 +4,7 @@ import Style from '../Input_Docs/InputBox.module.css'
 const InputBox = () => {
     return ( 
         <div>
-            <input type="text" placeholder='Input Text' />
+            <input className={Style.input_style} type="text" placeholder='Input Text' />
             <button type="button" className={Style.button_style}>ADD</button>
         </div>
      );
