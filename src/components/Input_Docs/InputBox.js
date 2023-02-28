821a3c970c97a71fb9b3ac5d94bbc1f053596598
KRMadhusanka99 <Kkrmadhu1999@gmail.com>
2023-02-28 12:00:25 +0530

    'addbuttonfunction'

--- src/components/Input_Docs/InputBox.js
+++ src/components/Input_Docs/InputBox.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import Style from '../Input_Docs/InputBox.module.css'
 
+// document.getElementById('textField').value //java script way to get a string
+
 const InputBox = () => {
+    //add a method to add button cllick
+    const clickHandler = () =>{
+        alert("Clicked");
+    }
+    
     return ( 
         <div className={Style.oute_div}>
             <input className={Style.input_style} type="text" placeholder='Input Text' />
-            <button type="button" className={Style.button_style}>ADD</button>
+            <button type="button" className={Style.button_style} onClick={clickHandler}>ADD</button>
         </div>
      );
 };
